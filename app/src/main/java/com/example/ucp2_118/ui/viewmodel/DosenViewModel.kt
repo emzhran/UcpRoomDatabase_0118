9065af4e8bbbd9b4d4701d34bfd85b564fb29cf3
emzhran <zachran750@gmail.com>
2024-12-18 23:21:11 +0700

    menambahkan data class DsnUIState dan FormErrorState pada DosenViewModel

--- app/src/main/java/com/example/ucp2_118/ui/viewmodel/DosenViewModel.kt
+++ app/src/main/java/com/example/ucp2_118/ui/viewmodel/DosenViewModel.kt
@@ -3,6 +3,22 @@ package com.example.ucp2_118.ui.viewmodel
 import com.example.ucp2_118.data.entity.Dosen
 
 
+data class DsnUIState(
+    val dosenEvent: DosenEvent = DosenEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    val snackbarMessage: String? = null
+)
+
+data class FormErrorState(
+    val nidn: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+){
+    fun isValid(): Boolean{
+        return nidn == null && nama == null && jenisKelamin == null
+    }
+}
+
 fun DosenEvent.toDosenEntity():Dosen = Dosen(
     nidn = nidn,
     nama = nama,
