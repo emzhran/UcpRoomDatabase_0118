fd171c4700c65f9b30319eaca03658fe88cfaf87
emzhran <zachran750@gmail.com>
2024-12-18 22:43:42 +0700

    menambahkan repository dosen dan matakuliah pada ContainerApp

--- app/src/main/java/com/example/ucp2_118/dependenciesinjection/ContainerApp.kt
+++ app/src/main/java/com/example/ucp2_118/dependenciesinjection/ContainerApp.kt
@@ -1,9 +1,21 @@
 package com.example.ucp2_118.dependenciesinjection
 
+import android.content.Context
+import com.example.ucp2_118.data.database.DatabaseKrs
+import com.example.ucp2_118.repository.LocalRepositoryDsn
+import com.example.ucp2_118.repository.LocalRepositoryMatkul
 import com.example.ucp2_118.repository.RepositoryDsn
 import com.example.ucp2_118.repository.RepositoryMatkul
 
 interface InterfaceContainerApp{
     val repositoryDsn: RepositoryDsn
     val repositoryMatkul: RepositoryMatkul
+}
+
+class ContainerApp (private val context: Context): InterfaceContainerApp{
+    override val repositoryDsn: RepositoryDsn by lazy {
+        LocalRepositoryDsn(DatabaseKrs.getDatabase(context).DosenDao()) }
+
+    override val repositoryMatkul: RepositoryMatkul by lazy {
+        LocalRepositoryMatkul(DatabaseKrs.getDatabase(context).MatakuliahDao()) }
 }
