8c15ef97a23a349064a67f4a06c252dd733cad5f
emzhran <zachran750@gmail.com>
2024-12-18 22:55:32 +0700

    pembuatan TopAppBar serta menambahkan parameter dan box

---
+++ app/src/main/java/com/example/ucp2_118/ui/customwidget/TopAppBar.kt
@@ -0,0 +1,23 @@
+package com.example.ucp2_118.ui.customwidget
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TopAppBar(
+    onBack:() -> Unit,
+    showBackButton: Boolean = true,
+    judul: String
+){
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        contentAlignment = Alignment.Center
+    ){}
+}
