62e11bef49f47f1595e6f6ab932a4084b347c519
emzhran <zachran750@gmail.com>
2024-12-19 16:19:26 +0700

    pembuatan AlamatNavigasi serta menambahkan objek DestniasiHome dan DestinasiDetail untuk dosen

---
+++ app/src/main/java/com/example/ucp2_118/navigation/AlamatNavigasi.kt
@@ -0,0 +1,16 @@
+package com.example.ucp2_118.navigation
+
+interface AlamatNavigasi {
+    val route : String
+
+    object DestinasiHomeDosen : AlamatNavigasi{
+        override val route = "home"
+    }
+
+    object DestinasiDetailDosen : AlamatNavigasi{
+        override val route = "detail"
+        const val NIDN = "nidn"
+        val routeWithArg = "$route/{$NIDN}"
+    }
+
+}
