d8db4c5c1d54d607d04f5ba19e94cc5d84136bfe
emzhran <zachran750@gmail.com>
2024-12-19 19:42:28 +0700

    pembuatan InsertDsnView untuk fungsi insert dosen

---
+++ app/src/main/java/com/example/ucp2_118/ui/view/dosen/InsertDsnView.kt
@@ -0,0 +1,172 @@
+package com.example.ucp2_118.ui.view.dosen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.room.util.TableInfo
+import com.example.ucp2_118.ui.customwidget.TopAppBar
+import com.example.ucp2_118.ui.viewmodel.DosenEvent
+import com.example.ucp2_118.ui.viewmodel.DosenViewModel
+import com.example.ucp2_118.ui.viewmodel.DsnUIState
+import com.example.ucp2_118.ui.viewmodel.FormErrorState
+import com.example.ucp2_118.ui.viewmodel.HomeDsnViewModel
+import com.example.ucp2_118.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun InsertDsnView(
+    onBack: ()-> Unit,
+    onNavigate: ()-> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DosenViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.uiState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.dosenEvent) {
+        uiState.snackbarMessage?.let { message->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+    Scaffold(
+        modifier = Modifier,
+        snackbarHost = {SnackbarHost(hostState = snackbarHostState)}
+    ) { padding->
+        Column(
+            modifier = Modifier.fillMaxSize()
+                .padding(padding)
+                .padding(15.dp)
+        ) {
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Dosen"
+            )
+            InsertBodyDsn(
+                uiState = uiState,
+                onValueChange = {updateEvent->
+                    viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    coroutineScope.launch { viewModel.saveData() }
+                    onNavigate
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun InsertBodyDsn(
+    modifier: Modifier = Modifier,
+    onValueChange: (DosenEvent) -> Unit,
+    uiState: DsnUIState,
+    onClick:()-> Unit
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        FormDosen(
+            dosenEvent = uiState.dosenEvent,
+            onValueChange = onValueChange,
+            errorState = uiState.isEntryValid,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Simpan")
+        }
+    }
+}
+
+@Composable
+fun FormDosen(
+    dosenEvent: DosenEvent = DosenEvent(),
+    onValueChange:(DosenEvent) -> Unit = {},
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+){
+    val jenisKelamin = listOf("Laki-Laki", "Perempuan")
+
+    Column(modifier = modifier.fillMaxWidth()){
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dosenEvent.nama,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nama = it))
+            },
+            label = { Text("Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan Nama") }
+        )
+        Text(
+            text = errorState.nama?:"",
+            color = Color.Red
+        )
+        OutlinedTextField(modifier =  modifier.fillMaxWidth(),
+            value = dosenEvent.nidn,
+            onValueChange = {
+                onValueChange(dosenEvent.copy(nidn = it))
+            },
+            label = { Text("NIDN") },
+            isError = errorState.nidn != null,
+            placeholder = { Text("Masukkan NIDN") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(
+            text = errorState.nidn?:"",
+            color = Color.Red
+        )
+
+        Spacer(modifier = Modifier.height(15.dp))
+        Text(text = "Jenis Kelamin")
+        Row (modifier = Modifier.fillMaxWidth())
+        {
+            jenisKelamin.forEach{jk->
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Start
+                ) {
+                    RadioButton(
+                        selected = dosenEvent.jenisKelamin == jk,
+                        onClick = {
+                            onValueChange(dosenEvent.copy(jenisKelamin = jk))
+                        },
+                    )
+                    Text(text = jk)
+                }
+            }
+        }
+    }
+}
