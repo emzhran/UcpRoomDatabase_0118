acb5c05d904daf5befdf46aab7fdfe29be617356
emzhran <zachran750@gmail.com>
2024-12-19 19:07:34 +0700

    menambahkan fungsi HomeDsnView dan BodyHomeDsnView pada HomeDsnView

--- app/src/main/java/com/example/ucp2_118/ui/view/dosen/HomeDsnView.kt
+++ app/src/main/java/com/example/ucp2_118/ui/view/dosen/HomeDsnView.kt
@@ -1,29 +1,135 @@
 package com.example.ucp2_118.ui.view.dosen
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Face
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Snackbar
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_118.data.entity.Dosen
+import com.example.ucp2_118.ui.customwidget.TopAppBar
+import com.example.ucp2_118.ui.viewmodel.HomeDsnViewModel
+import com.example.ucp2_118.ui.viewmodel.HomeUiState
+import com.example.ucp2_118.ui.viewmodel.PenyediaViewModel
+
+@Composable
+fun HomeDsnView(
+    viewModel: HomeDsnViewModel = viewModel(factory =  PenyediaViewModel.Factory),
+    onAddDsn: ()-> Unit = {},
+    onDetailClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar ={
+            TopAppBar(
+                onBack = {},
+                showBackButton = false,
+                judul = "Daftar Dosen"
+            )
+        },
+        floatingActionButton ={
+            FloatingActionButton(
+                onClick = onAddDsn,
+                shape = MaterialTheme.shapes.medium,
+                modifier = modifier.padding(18.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Dosen"
+                )
+            }
+        }
+    ) {
+        innerpadding-> val homeUiState by viewModel.homeUiState.collectAsState()
+
+        BodyHomeDsnView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = modifier.padding(innerpadding)
+        )
+    }
+}
+
+
+@Composable
+fun BodyHomeDsnView(
+    homeUiState: HomeUiState,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when{
+        homeUiState.isLoading->{
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeUiState.listDsn.isEmpty()->{
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data mahasiswa.",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(18.dp)
+                )
+            }
+        }
+        else->{
+            ListDosen(
+                listDsn = homeUiState.listDsn,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+
 
 @Composable
 fun ListDosen(
