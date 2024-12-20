ca3596a278fae9d9972c3d97fb633418eb2bef65
emzhran <zachran750@gmail.com>
2024-12-20 15:01:50 +0700

    perbaikan code pada HomeMatkulView

--- app/src/main/java/com/example/ucp2_118/view/matakuliah/HomeMatkulView.kt
+++ app/src/main/java/com/example/ucp2_118/view/matakuliah/HomeMatkulView.kt
@@ -1,10 +1,34 @@
 package com.example.ucp2_118.view.matakuliah
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.PlayArrow
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.ucp2_118.data.entity.MataKuliah
 import com.example.ucp2_118.repository.RepositoryMatkul
 import com.example.ucp2_118.ui.viewmodel.HomeUiState
+import com.example.ucp2_118.ui.viewmodel.MatkulEvent
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -14,43 +38,53 @@ import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
 
-class HomeMatkulView(
-    private val repositoryMatkul: RepositoryMatkul
-): ViewModel(){
-
-    val HomeUIState: StateFlow<HomeUIState> = repositoryMatkul.getAllMatakuliah()
-        .filterNotNull()
-        .map {
-            HomeUIState(
-                listMatkul = it.toList(),
-                isLoading = false
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CardMatkul(
+    matkul : MataKuliah,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {}
+){
+    Card(
+        onClick = onClick,
+        modifier = Modifier.fillMaxWidth()
+            .padding(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(imageVector = Icons.Filled.AddCircle, contentDescription = "")
+            Spacer(modifier = Modifier.padding(5.dp))
+            Text(
+                text = matkul.kode,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
             )
         }
-        .onStart {
-            emit(HomeUIState(isLoading = true))
-            delay(900)
-        }
-        .catch {
-            emit(
-                HomeUIState(
-                    isLoading = false,
-                    isError = true,
-                    errorMessage = it.message?: "Terjadi Kesalahan"
-                )
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(imageVector = Icons.Filled.Create, contentDescription = "")
+            Spacer(modifier = Modifier.padding(5.dp))
+            Text(
+                text = matkul.nama,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
             )
         }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5000),
-            initialValue = HomeUIState(
-                isLoading = true
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(imageVector = Icons.Filled.Person, contentDescription = "")
+            Spacer(modifier = modifier.padding(5.dp))
+            Text(
+                text = matkul.dosenPengampu,
+                fontWeight = FontWeight.Bold,
+                fontSize = 16.sp
             )
-        )
+        }
+    }
 }
-
-data class HomeUIState(
-    val listMatkul: List<MataKuliah> = listOf(),
-    val isLoading: Boolean = false,
-    val isError: Boolean = false,
-    val errorMessage: String = ""
-)
