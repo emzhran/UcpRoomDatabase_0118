1a7a552c0ecb58cbe82b8d8fe5f2b5d67863da7e
emzhran <zachran750@gmail.com>
2024-12-21 15:00:44 +0700

    penambahan fungsi pada HomeMatkulView

--- app/src/main/java/com/example/ucp2_118/view/matakuliah/HomeMatkulView.kt
+++ app/src/main/java/com/example/ucp2_118/view/matakuliah/HomeMatkulView.kt
@@ -1,11 +1,15 @@
 package com.example.ucp2_118.view.matakuliah
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Create
@@ -13,11 +17,21 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material3.Card
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
@@ -25,10 +39,16 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_118.data.entity.MataKuliah
 import com.example.ucp2_118.repository.RepositoryMatkul
+import com.example.ucp2_118.ui.customwidget.TopAppBar
+import com.example.ucp2_118.ui.viewmodel.DetailMatkulViewModel
+import com.example.ucp2_118.ui.viewmodel.HomeMatkulState
+import com.example.ucp2_118.ui.viewmodel.HomeMatkulViewModel
 import com.example.ucp2_118.ui.viewmodel.HomeUiState
 import com.example.ucp2_118.ui.viewmodel.MatkulEvent
+import com.example.ucp2_118.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
@@ -37,6 +57,121 @@ import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.onStart
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeMatkulView(
+    viewModel: HomeMatkulViewModel = viewModel(factory = PenyediaViewModel.Factory),
+    onAddMatkul:()-> Unit ={},
+    onDetailClick:(String)-> Unit = {},
+    modifier: Modifier = Modifier
+){
+    Scaffold(
+        topBar ={
+            TopAppBar(
+                onBack = {},
+                showBackButton = false,
+                judul = "Daftar Matakuliah"
+            )
+        },
+        floatingActionButton ={
+            FloatingActionButton(
+                onClick = onAddMatkul,
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(16.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Tambah Matakuliah"
+                )
+            }
+        }
+    ) { innerpadding->
+        val homeUiState by viewModel.HomeMatkulState.collectAsState()
+
+        BodyMatkulView(
+            homeUiState = homeUiState,
+            onClick = {
+                onDetailClick(it)
+            },
+            modifier = Modifier.padding(innerpadding)
+        )
+    }
+}
+
+
+@Composable
+fun BodyMatkulView(
+    homeUiState: HomeMatkulState,
+    onClick: (String) -> Unit = {},
+    modifier: Modifier = Modifier
+){
+    val coroutineScope = rememberCoroutineScope()
+    val snackbarHostState = remember { SnackbarHostState() }
+    when{
+        homeUiState.isLoading->{
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                CircularProgressIndicator()
+            }
+        }
+        homeUiState.isError->{
+            LaunchedEffect(homeUiState.errorMessage) {
+                homeUiState.errorMessage?.let { message ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(message)
+                    }
+                }
+            }
+        }
+        homeUiState.listMatkul.isEmpty()->{
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ){
+                Text(
+                    text = "Tidak ada data matakuliah.",
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+        else->{
+            listMatkul(
+                listMatkul = homeUiState.listMatkul,
+                onClick = {
+                    onClick(it)
+                    println(
+                        it
+                    )
+                },
+                modifier = modifier
+            )
+        }
+    }
+}
+
+@Composable
+fun listMatkul(
+    listMatkul:List<MataKuliah>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = {}
+){
+    LazyColumn(modifier = Modifier) {
+        items(
+            items = listMatkul,
+            itemContent = {matkul->
+                CardMatkul(
+                    matkul = matkul,
+                    onClick = {onClick(matkul.kode)}
+                )
+            }
+        )
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
