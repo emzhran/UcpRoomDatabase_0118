14c7a89d6a977fb62e3a284fe77eb05101f42f23
emzhran <zachran750@gmail.com>
2024-12-21 13:52:55 +0700

    penambahan fungsi pada InsertMatkulView

--- app/src/main/java/com/example/ucp2_118/view/matakuliah/InsertMatkulView.kt
+++ app/src/main/java/com/example/ucp2_118/view/matakuliah/InsertMatkulView.kt
@@ -4,8 +4,11 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -13,12 +16,16 @@ import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -26,8 +33,90 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2_118.repository.RepositoryDsn
+import com.example.ucp2_118.ui.customwidget.TopAppBar
 import com.example.ucp2_118.ui.viewmodel.MatkulErrorState
 import com.example.ucp2_118.ui.viewmodel.MatkulEvent
+import com.example.ucp2_118.ui.viewmodel.MatkulUiState
+import com.example.ucp2_118.ui.viewmodel.MatkulViewModel
+import com.example.ucp2_118.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+
+@Composable
+fun InsertMatkulView(
+    onBack:()-> Unit,
+    onNavigate:()-> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: MatkulViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.uiState
+    val snackbarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.matkulEvent) {
+        uiState.snackbarMessage?.let { message->
+            coroutineScope.launch {
+                snackbarHostState.showSnackbar(message)
+                viewModel.resetSnackBarMessage()
+            }
+        }
+    }
+    Scaffold(
+        modifier = Modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) }
+    ) {padding->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(15.dp)
+        ) {
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Matakuliah"
+            )
+            InsertBodyMatkul(
+                uiState = uiState,
+                onValueChange ={updateEvent->
+                    viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        viewModel.saveData()
+                    }
+                    onNavigate
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun InsertBodyMatkul(
+    modifier: Modifier = Modifier,
+    onValueChange: (MatkulEvent) -> Unit,
+    uiState: MatkulUiState,
+    onClick: () -> Unit
+){
+    Column(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) { FormMatakuliah(
+        matkulEvent = uiState.matkulEvent,
+        onValueChange = onValueChange,
+        errorState = uiState.isEntryValid,
+        modifier = Modifier.fillMaxWidth()
+    )
+        Button(
+            onClick = onClick,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Simpan")
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
